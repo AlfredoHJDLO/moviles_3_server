59e75bafd1cfb018930430f418088f75c96a601a
Eduardo Rojas <rore030616@gs.utm.mx>
2025-06-08 14:40:24 -0600

    Feat: Usuario, Favoritos, Carrito y Compras

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, DateTime
+from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import shutil
 import os
 from typing import List
-
+from pydantic import EmailStr
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
 
-
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -41,6 +44,31 @@ class Tienda(Base):
     category = Column(String(100))
     media = Column(String(1000))  # solo guardaremos el nombre del archivo o la ruta
 
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50))
+    email = Column(String(255), unique=True, index=True)
+    password = Column(String(255))  # Idealmente con hash
+
+class Carrito(Base):
+    __tablename__ = 'carrito'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('usuarios.id'))
+    producto_id = Column(Integer, ForeignKey('tienda.id'))
+
+class Favorito(Base):
+    __tablename__ = 'favoritos'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('usuarios.id'))
+    producto_id = Column(Integer, ForeignKey('tienda.id'))
+
+class Compra(Base):
+    __tablename__ = 'compras'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('usuarios.id'))
+    producto_id = Column(Integer, ForeignKey('tienda.id'))
+    fecha = Column(DateTime, default=datetime.now)
 Base.metadata.create_all(bind=engine)
 
 # Ruta estática para servir imágenes
@@ -59,7 +87,6 @@ async def create_product(
         saved_files = []
         # Guardar imagen en la carpeta local
         for file in media:
-            ext = os.path.splitext(file.filename)[1]
             path = f"images/{file.filename}"  # puedes renombrar para evitar duplicados
             with open(path, "wb") as buffer:
                 shutil.copyfileobj(file.file, buffer)
@@ -110,7 +137,6 @@ def get_producto(id: int):
 def get_all_productos():
     db = SessionLocal()
     productos = db.query(Tienda).all()
-    media_urls = productos.media.split(",")
     return [
         {
             "id": producto.id,
@@ -118,7 +144,85 @@ def get_all_productos():
             "description": producto.description,
             "price": producto.price,
             "category": producto.category,
-            "image": media_urls,
+            "media": producto.media.split(","),
         }
         for producto in productos
     ]
+
+# * Autenticacion
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+@app.post("/register")
+def register_user(name: str = Form(...) ,email: EmailStr = Form(...), password: str = Form(...)):
+    db = SessionLocal()
+    hashed = hash_password(password)
+    exist = db.query(Usuario).filter(Usuario.email == email).first()
+    if exist:
+        raise HTTPException(status_code= 404, detail="EL usuario con ese correo ya existe")
+    user = Usuario(name = name, email=email, password=hashed)
+    db.add(user)
+    db.commit()
+    return {"msg": "Usuario registrado"}
+
+
+#* Favoritos
+@app.post("/favoritos/")
+def add_favorito(user_id: int, producto_id: int):
+    db = SessionLocal()
+    fav = Favorito(user_id=user_id, producto_id=producto_id)
+    db.add(fav)
+    db.commit()
+    return {"msg": "Agregado a favoritos"}
+
+@app.get("/favoritos/{user_id}")
+def get_favoritos(user_id: int):
+    db = SessionLocal()
+    favs = db.query(Favorito).filter(Favorito.user_id == user_id).all()
+    return [{"producto_id": f.producto_id} for f in favs]
+
+@app.delete("/favoritos/{user_id}")
+def delete_favoritos(user_id: int):
+    db = SessionLocal()
+    exist = db.query(Usuario).filter(Usuario.id == user_id).first()
+    if not exist:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    db.delete()
+    db.commit()
+    return {"message": "usuario eliminado"}
+
+#* Carrito
+@app.post("/carrito/")
+def add_to_cart(user_id: int, producto_id: int):
+    db = SessionLocal()
+    item = Carrito(user_id=user_id, producto_id=producto_id)
+    db.add(item)
+    db.commit()
+    return {"msg": "Producto agregado al carrito"}
+
+@app.get("/carrito/{user_id}")
+def get_cart(user_id: int):
+    db = SessionLocal()
+    items = db.query(Carrito).filter(Carrito.user_id == user_id).all()
+    return [{"producto_id": i.producto_id} for i in items]
+
+#* Comprar
+@app.post("/comprar/")
+def comprar(user_id: int):
+    db = SessionLocal()
+    items = db.query(Carrito).filter(Carrito.user_id == user_id).all()
+    for item in items:
+        compra = Compra(user_id=user_id, producto_id=item.producto_id)
+        db.add(compra)
+        db.delete(item)  # eliminar del carrito
+    db.commit()
+    return {"msg": "Compra realizada"}
+
+@app.get("/compras/{user_id}")
+def get_compras(user_id: int):
+    db = SessionLocal()
+    compras = db.query(Compra).filter(Compra.user_id == user_id).all()
+    return [{"producto_id": c.producto_id, "fecha": c.fecha} for c in compras]
